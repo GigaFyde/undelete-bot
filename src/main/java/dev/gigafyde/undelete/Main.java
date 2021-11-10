b66acc81f6313d4a42a0c62e80bff15a2090a38e
Laurant Marijnissen <laurant@gigafyde.dev>
2021-11-10 09:00:28 +0100

    First pass at role add delay timer

--- src/main/java/dev/gigafyde/undelete/Main.java
+++ src/main/java/dev/gigafyde/undelete/Main.java
@@ -4,8 +4,10 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
+    public static JDA jda;
     public static void main(String[] args) throws Exception {
-        JDA jda = JDABuilder.createDefault(System.getenv("TOKEN")).addEventListeners(new DeleteListener()).build();
+        jda = JDABuilder.createDefault(System.getenv("TOKEN"))
+                .addEventListeners(new DeleteListener(), new RoleTimer()).build();
         jda.awaitReady();
     }
 }
---
+++ src/main/java/dev/gigafyde/undelete/RoleTimer.java
@@ -0,0 +1,36 @@
+package dev.gigafyde.undelete;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class RoleTimer extends ListenerAdapter {
+    Role role = Main.jda.getRoleById("794198246544900136");
+
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        addMemberRoleAfterDelay(event.getMember());
+    }
+
+    public void addMemberRoleAfterDelay(Member member) {
+        TimerTask task = new TimerTask() {
+            public void run() {
+                try {
+                    Guild guild = member.getGuild();
+                    guild.addRoleToMember(member, role).queue();
+                } catch (Exception ignored) {
+
+                }
+            }
+        };
+        Timer timer = new Timer("RoleTimer");
+        long delay = TimeUnit.HOURS.toMillis(6);
+        timer.schedule(task, delay);
+
+    }
+}
