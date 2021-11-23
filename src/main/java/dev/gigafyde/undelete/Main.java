c72e3590238b2f64f30885772dce19b2effb3dfd
Laurant Marijnissen <laurant@gigafyde.dev>
2021-11-23 21:33:32 +0100

    Add intents to fix ignored events

--- src/main/java/dev/gigafyde/undelete/Main.java
+++ src/main/java/dev/gigafyde/undelete/Main.java
@@ -3,11 +3,13 @@ package dev.gigafyde.undelete;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main {
     public static Role role;
     public static void main(String[] args) throws Exception {
         JDA jda = JDABuilder.createDefault(System.getenv("TOKEN"))
+                .setEnabledIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.DIRECT_MESSAGES)
                 .addEventListeners(new DeleteListener(), new JoinListener(), new RoleTimer()).build();
         jda.awaitReady();
         role = jda.getRoleById("794198246544900136");
