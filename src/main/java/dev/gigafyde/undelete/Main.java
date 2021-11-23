fecebcb782a90f82a711892354f815c88bac84ff
Laurant Marijnissen <laurant@gigafyde.dev>
2021-11-23 21:24:20 +0100

    Also add roletimer with a fix

--- src/main/java/dev/gigafyde/undelete/Main.java
+++ src/main/java/dev/gigafyde/undelete/Main.java
@@ -2,12 +2,14 @@ package dev.gigafyde.undelete;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Role;
 
 public class Main {
-    public static JDA jda;
+    public static Role role;
     public static void main(String[] args) throws Exception {
-        jda = JDABuilder.createDefault(System.getenv("TOKEN"))
-                .addEventListeners(new DeleteListener(), new JoinListener()).build();
+        JDA jda = JDABuilder.createDefault(System.getenv("TOKEN"))
+                .addEventListeners(new DeleteListener(), new JoinListener(), new RoleTimer()).build();
         jda.awaitReady();
+        role = jda.getRoleById("794198246544900136");
     }
 }
